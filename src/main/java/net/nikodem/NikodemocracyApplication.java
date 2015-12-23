e15235fd60398c72cb055ffe00fdcb27f67e8127
Peter Nikodem <peternikodemjr@gmail.com>
2015-12-23 17:55:29 +0100

    Initial commit.

---
+++ src/main/java/net/nikodem/NikodemocracyApplication.java
@@ -0,0 +1,11 @@
+package net.nikodem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NikodemocracyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NikodemocracyApplication.class, args);
+    }
+}
